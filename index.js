6a8a8817e130a7fd9b3a1d3087566b2dfd03c70f
Rinaldy <64340069+rinaldycodes@users.noreply.github.com>
2023-12-05 09:56:34 +0700

    Update index.js
    add 2 function

--- index.js
+++ index.js
@@ -62,3 +62,58 @@ function validateEmail(email) {
 function generateRandomNumber(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
+export const getCurrentDateTimeUTC = () => {
+  const currentDateTime = new Date(); // Mendapatkan tanggal dan waktu saat ini
+  const currentDateTimeUTC = currentDateTime.toISOString(); // Mengonversi ke format UTC
+
+  return currentDateTimeUTC;
+};
+
+const checkUTC = async () => {
+      
+     const response = await fetch(`https://worldtimeapi.org/api/timezone/Etc/UTC`,
+           {
+           method: 'GET',
+           headers: {
+               Accept: 'application/json',
+               'Content-Type': 'application/json',
+           },
+       })
+
+       const res = await response.json();
+
+
+       if ( res['error'] ) {
+           return {
+             status: '400',
+             message: 'error UTC',
+           }
+       } else {
+         const resUTC = res;
+         const tz = getCurrentDateTimeUTC();
+
+         if ( resUTC['utc_datetime'] ) {
+           const UTCFormate = resUTC.utc_datetime.substring(0,19).replace('T',' ');
+           const TZFormate = tz.substring(0,19).replace('T',' ');
+ 
+           const plusUTC = addMinute(UTCFormate, 1);
+           const minusUTC = minusMinute(UTCFormate, 1);
+ 
+           if ( TZFormate >= minusUTC && TZFormate <= plusUTC ) {
+               console.log('no alert is ok')
+               return {
+                 status: '200',
+                 message: 'UTC Benar',
+               }
+           } else {
+             return {
+               status: '201',
+               message: 'Pastikan pengaturan wajtu sudah benar!',
+             }
+           }
+         } else {
+           return res;
+         }
+       }
+   }
